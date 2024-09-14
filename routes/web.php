0848f3f8db1295805668345c48ac416fdc1c72b2
khaingmyozaw <kmyozaw.dev@gmail.com>
2024-09-14 23:50:42 +0630

    register form pending

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('partials.index');
 });
+Route::get('/register', function() {
+    return view('partials.forms.register');
+});
